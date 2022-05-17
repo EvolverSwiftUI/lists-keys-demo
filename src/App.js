06d34de2c16c208c0880916f413eae1b848ad289
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-05-17 18:34:26 +0530

    React JS intro and functional component implementation done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import UserProfile from './components/UserProfile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: "https://picsum.photos/200",
+    name: "Jhon Andrew",
+    role: "Software Engineer"
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: "https://picsum.photos/300",
+    name: "Smith Wilson",
+    role: "Sr. Software Engineer"
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: "https://picsum.photos/400",
+    name: "Mark Jukarburg",
+    role: "Jr. Software Engineer"
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: "https://picsum.photos/500",
+    name: "Robot C Warnew",
+    role: "Tech Lead"
+  }
+]
+
+// by normal function format
+// function App() {
+//   return (
+//     <UserProfile />
+//   );
+// }
+
+// by arrow function format
+const App = () => (
+  <div className="list-container">
+    <h1 className="title"> Users List</h1>
+    <ul>
+        {/* <UserProfile userDetails={userDetailsList[0]}/>
+        <UserProfile userDetails={userDetailsList[1]}/>
+        <UserProfile userDetails={userDetailsList[2]}/>
+        <UserProfile userDetails={userDetailsList[3]}/> */}
+        {
+          userDetailsList.map((eachItem) => (
+            <UserProfile userDetails={eachItem} key={eachItem.uniqueNo}/>
+          ))
+        }
+    </ul>
+    
+  </div>
+)
 
 export default App;
